5384b3fa77c7c14de3a96dbea0e46f1fa81090a5
monica hernandez <moni_g_hernandez@hotmail.com>
2023-12-11 23:31:40 -0600

    correccion1112

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//este codigo abre el formulario de usuario//
-
+//Este codigo abre el formulario de usuario//
 const abrir = document.querySelector('.profile__edit');
 const overlay = document.querySelector('.overlay');
 const popup = document.querySelector('.popup');
@@ -24,36 +23,141 @@ cerrar.addEventListener('click', function () {
   popup.classList.remove('active');
 });
 
+//Guarda los nuevos datos
 form.addEventListener('submit', function (event) {
-  event.preventDefault(); // Prevents the default form submission behavior
+  event.preventDefault();
   const nameInput = document.getElementById('name');
   const placeInput = document.getElementById('place');
 
-  // Set the profile title and subtitle based on the form inputs
+  //Titulo y subtitulo
   profileTitle.innerText = nameInput.value;
   profileSubtitle.innerText = placeInput.value;
 
-  // Close the popup
+  //Cierra popup
   overlay.classList.remove('active');
   popup.classList.remove('active');
 });
 
+//cartas nuevas
+  const elementsData = [
+    {
+        imageSrc: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+        title: "Valle de Yosemite"
+    },
+    {
+        imageSrc: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+        title: "Lago Louise"
+    },
+    {
+        imageSrc: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+        title: "Montañas Calvas"
+    },
+    {
+        imageSrc: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+        title: "Latemar"
+    },
+    {
+        imageSrc: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+        title: "Vanois National Park"
+    },
+    {
+        imageSrc: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+        title: "Lago di Braies"
+    }
+];
 
+  const elementTemplate = document.getElementById('element-template').content;
+  const elementsContainer = document.querySelector('.elements');
 
-//este codigo abre el modal//
+  elementsData.forEach(data => {
+      const element = document.importNode(elementTemplate, true);
 
-const imagesToOpen = document.querySelectorAll('.element__image');
-const closeModalBtn = document.querySelector('.modal__close');
-const modal = document.querySelector('.modal');
-const modalImg = document.querySelector('.modal__image');
+      element.querySelector('.element__image').src = data.imageSrc;
+      element.querySelector('.element__title').textContent = data.title;
 
-imagesToOpen.forEach(function(image) {
-  image.addEventListener('click', function() {
-    modal.style.display = 'block';
-    modalImg.src = image.src;
+      elementsContainer.appendChild(element);
+  });
+
+//codigo del modal//
+  const imagesToOpen = document.querySelectorAll('.element__image');
+  const closeModalBtn = document.querySelector('.modal__close');
+  const modal = document.querySelector('.modal');
+  const modalImg = document.querySelector('.modal__image');
+  const modalText = document.querySelector('.modal__text');
+
+  imagesToOpen.forEach(function(image, index) {
+    image.addEventListener('click', function() {
+      modal.style.display = 'block';
+      const dataIndex = index;
+      const imageSrc = elementsData[dataIndex].imageSrc;
+      const imageTitle = elementsData[dataIndex].title;
+      modalImg.src = imageSrc;
+      modalText.textContent = imageTitle;
+    });
+  });
+
+  closeModalBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+  });
+
+// codigo para abrir el formulario add
+const openAdd = document.querySelector('.profile__button');
+const add = document.querySelector('.add');
+const popupAdd = document.querySelector('.add__popup');
+const closeAdd = document.querySelector('.add__close');
+const formAdd = document.querySelector('.add__form');
+const agregarButton = document.getElementById('agregar');
+
+openAdd.addEventListener('click', function () {
+  add.classList.add('active');
+  popupAdd.classList.add('active');
+});
+
+closeAdd.addEventListener('click', function () {
+  add.classList.remove('active');
+  popupAdd.classList.remove('active');
+});
+
+//campos predeterminados
+const titleInput = document.getElementById('title');
+const urlInput = document.getElementById('url');
+titleInput.value = 'Título';
+urlInput.value = 'URL de la imagen';
+
+//Guarda los nuevos datos
+  agregarButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const newTitle = titleInput.value;
+    const newImageUrl = urlInput.value;
+
+    const elementTemplate = document.getElementById("element-template").content;
+    const newElement = document.importNode(elementTemplate, true);
+    newElement.querySelector(".element__image").src = newImageUrl;
+    newElement.querySelector(".element__title").textContent = newTitle;
+
+    elementsContainer.appendChild(newElement);
+    titleInput.value = "";
+    urlInput.value = "";
+
+    add.classList.remove('active');
+    popupAdd.classList.remove('active');
+  });
+
+//Cdigo para like
+const likeElements = document.querySelectorAll('.element__like');
+
+likeElements.forEach((likeElement, index) => {
+  likeElement.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("element__like_active");
   });
 });
 
-closeModalBtn.addEventListener('click', function() {
-  modal.style.display = 'none';
-});
+//Codigo para eliminar imagenes
+  const trashElements = document.querySelectorAll('.element__trash');
+  const cardElements = document.querySelectorAll('.element');
+
+  trashElements.forEach((trashElement, index) =>
+    trashElement.addEventListener('click', () => {
+      cardElements[index].style.display = 'none';
+    })
+  );
